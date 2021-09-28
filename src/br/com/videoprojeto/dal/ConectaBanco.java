8b58f73c0ea1e39c9e660b8e1153879a08297cfd
lessacaires <lessacaires@gmail.com>
2021-09-28 16:15:37 -0300

    Add files via upload

--- src/br/com/videoprojeto/dal/ConectaBanco.java
+++ src/br/com/videoprojeto/dal/ConectaBanco.java
@@ -5,10 +5,33 @@
  */
 package br.com.videoprojeto.dal;
 
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 3164527
  */
 public class ConectaBanco {
+    public Statement stm;//responsavel por realizar pesquisar dentro do banco de dados
+    public ResultSet rs;//responsavel por armazenar o resultado de uma pesquisa passada para o statement 
+    private String driver = "org.postgresql.Driver";
+    private String caminho = "jdbc:postgresql://localhost:5432/sistemavideoaula";
+    private String usuario = "postgres";
+    private String senha = "135167";
+    public Connection conn;//responsavel por realizar a conexao com o banco de dados
     
+    //Método responsável por realizar a conexão com o banco.
+    public void conexao(){
+        
+        try{
+            System.setProperty("jdbc.driver", driver);
+            conn = DriverManager.getConnection(caminho, usuario, senha);
+            if(conn != null){
+                JOptionPane.showMessageDialog(null, "banco de dados conectado com sucesso.");
+            }
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Erro ao conectar ao banco de dados");
+        }
+    }
 }
